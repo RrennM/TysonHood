8c032c37f3a3a2a2a98cc8f8572e1d550750afaa
TysonHH <tysonhood@gmail.com>
2017-08-31 01:03:46 +0000

    drop db and add description to schema

--- v2/app.js
+++ v2/app.js
@@ -19,17 +19,33 @@ app.set("view engine", "ejs");
 // Schema set up
 var adventureSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Adventure = mongoose.model("Adventure", adventureSchema);
 
-// Root Route
+// Adventure.create(
+//     {
+//         name: "Blue Pool", 
+//         image: "https://scontent-sea1-1.xx.fbcdn.net/v/t31.0-8/18278902_10209177208042950_1133499116197092643_o.jpg?oh=6db1bda102f36918dc543cbb2d6800d7&oe=5A300743",
+//         description: "Serene pool with a special, toxic algae which makes this look like nothing you've ever seen!"
+        
+//     }, function(err, adventure) {
+//         if(err) {
+//             console.log(err);
+//         } else {
+//             console.log("Newly created campground");
+//             console.log(adventure);
+//         }
+//     });
+
+// Root Route/Landing page
 app.get("/", function(req, res) {
    res.render("landing"); 
 });
 
-// POST route
+// INDEX route - Show all adventures
 app.post("/adventures", function(req, res) {
     // get data from form and add to adventures array
     var name = req.body.name;
@@ -46,7 +62,7 @@ app.post("/adventures", function(req, res) {
     })
 });
 
-// adventures page
+// CREATE route - add new adventure to db
 app.get("/adventures", function(req, res) {
     // get all adventures from DB
     Adventure.find({}, function(err, allAdventures) {
@@ -58,9 +74,14 @@ app.get("/adventures", function(req, res) {
     });
 });
 
-// Page to add new adventures
+// NEW route - show form to create a new adventure
 app.get("/adventures/new", function(req, res) {
     res.render("new")
+});
+
+// SHOW route - shows info about one adventure
+app.get("/adventures/:id", function(req, res) {
+    res.send("This will be the show page!");
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
